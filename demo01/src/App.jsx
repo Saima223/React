78e4a1249a2fd16858285e05f2d7787faca1cb3e
Saima223 <siddiqui22062004@gmail.com>
2026-05-05 10:28:20 +0530

    minor changes

--- demo01/src/App.jsx
+++ demo01/src/App.jsx
@@ -1,41 +1,20 @@
-import { useState, useEffect } from "react";
-import User from "./User";
+import { useState } from "react";
 
 function App() {
-  const [name, setName] = useState("Loading...");
+  const [text, setText] = useState("");
   const [count, setCount] = useState(0);
-
-  // 1️⃣ Runs on EVERY render
-  useEffect(() => {
-    console.log("Runs on every render");
-  });
-
-  // 2️⃣ Runs ONLY once (API call)
-  useEffect(() => {
-    console.log("Runs only once");
-
-    fetch("https://jsonplaceholder.typicode.com/users/1")
-      .then((res) => res.json())
-      .then((data) => {
-        setName(data.name);
-      });
-  }, []);
-
-  // 3️⃣ Runs when count changes
-  useEffect(() => {
-    console.log("Count changed:", count);
-  }, [count]);
 
   return (
     <div>
-      <h1>useEffect Practice</h1>
+      <input onChange={(e) => setText(e.target.value)} />
+
+      <h2>{text}</h2>
+
+      <h1>{count}</h1>
 
       <button onClick={() => setCount(count + 1)}>
-        Count: {count}
+        Increase
       </button>
-
-      {/* Props */}
-      <User username={name} />
     </div>
   );
 }
--- demo01/src/User.jsx
+++
@@ -1,9 +0,0 @@
-function User(props) {
-  return (
-    <div>
-      <h2>User Name: {props.username}</h2>
-    </div>
-  );
-}
-
-export default User;
